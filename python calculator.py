5d70822b86c413f36f29780f3557d55ca35f9901
AnchalGupta1117 <mehakanchal1068@gmail.com>
2024-07-06 15:02:27 +0530

    adding constructor

--- python calculator.py
+++ python calculator.py
@@ -1,6 +1,20 @@
+# Tk: This is the main window of the Tkinter application.
+# Entry: This is a widget that allows the user to enter a single line of text.
+# Button: This widget is used to create a button that the user can click.
+# StringVar: This is a Tkinter variable class that provides a way to manage the value of a widget, such as text in an Entry widget.
+
 from tkinter import Tk,Entry, Button , StringVar
 
+class Calculator:
+    def __init__(self,master):
+        master.title("Calculator")
+        master.geometry("357x420+0+0")
+        master.config(bg="gray")
+        master.resizable(False,False) 
 
+def show(self,value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
 
 root=Tk()
 Calculator=Calculator(root)
